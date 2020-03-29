2060c64b16458d5144c0f1f528d73c65639592c4
Gavin <gavchan@users.noreply.github.com>
2020-03-29 16:57:00 +0800

    Add advisory model

--- backend/cmsinv/models.py
+++ backend/cmsinv/models.py
@@ -28,12 +28,35 @@ class SupplierManufacturer(models.Model):
     class Meta:
         managed = False
         db_table = 'supplier_manufacturer'
+        app_label = 'cmsinv'
 
     def __str__(self):
         return '{} | {} - {}'.format(
             self.id, self.name, self.supp_type
         )
 
+class Advisory(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    version = models.BigIntegerField(default=0)
+    alias = models.CharField(unique=True, max_length=255)
+    created_by_cmsuser_id = models.BigIntegerField(db_column='created_by_id', blank=True, null=True)
+    date_created = models.DateTimeField()
+    description = models.TextField()
+    description_chinese = models.TextField(blank=True, null=True)
+    is_public = models.BooleanField()
+    last_updated = models.DateTimeField()
+    updated_by = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'advisory'
+        app_label = 'cmsinv'
+
+    def __str__(self):
+        return '{} | {} / {}'.format(
+            self.alias, self.description_chinese, self.description
+        )
+        
 class Instruction(models.Model):
     """CMS table for medication use instruction templates"""
     id = models.BigAutoField(primary_key=True)
@@ -50,6 +73,7 @@ class Instruction(models.Model):
     class Meta:
         managed = False
         db_table = 'instruction'
+        app_label = 'cmsinv'
 
     def __str__(self):
         return '{} | {} / {}'.format(
