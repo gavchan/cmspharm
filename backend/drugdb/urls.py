f45ef0644859b6536cb4838538ba51cdc4d10d20
Gavin <gavchan@users.noreply.github.com>
2020-04-16 21:44:08 +0800

    Basic template view for registered drug list

---
+++ backend/drugdb/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'drugdb'
+urlpatterns = [
+    path('drugs', views.RegisteredDrugList.as_view(), name='RegisteredDrugList'),
+]
--- backend/drugdb/views.py
+++ backend/drugdb/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views import generic
+#from django.shortcuts import get_object_or_404, render
+from .models import (
+    RegisteredDrug,
+    Company,
+)
 
-# Create your views here.
+class RegisteredDrugList(generic.ListView):
+    template_name = 'drugdb/drug_list.html'
+    model = RegisteredDrug
+    context_object_name = 'drug_list'
+
+    def get_queryset(self):
+        """Return 20 drugs."""
+        return RegisteredDrug.objects.all()[:20]
+
+class CompanyList(generic.ListView):
+    template_name = 'drugdb/company_list.html'
+    model = Company
+    context_object_name = 'company_list'
+
+    def get_queryset(self):
+        """Return 20 companies."""
+        return Company.objects.all()[:20]
