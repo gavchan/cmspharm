0578287120a989c3c1be09bdf4a02b4417eeef33
Gavin <gavchan@users.noreply.github.com>
2020-06-07 14:29:06 +0800

    Allow external connection

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ with open('dj_secret.key') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
