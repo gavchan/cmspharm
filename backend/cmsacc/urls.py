3dcf5499a35858c576bac1e07f71a45a621c2e50
Gavin <gavchan@users.noreply.github.com>
2021-01-25 23:30:45 +0800

    Remove cashbook and minor fixes

--- backend/cmsacc/urls.py
+++ backend/cmsacc/urls.py
@@ -4,5 +4,4 @@ from . import views
 app_name = 'cmsacc'
 urlpatterns = [
     path('bills/today/', views.BillToday.as_view(), name='BillToday'),
-    path('cashbook/', views.CashbookToday.as_view(), name='CashbookToday'),
     ]
